3271f3b2939cab3f3c081317d4ea7db41fd16bab
Yashodeep Joshi <96687722+Yashodeepj7@users.noreply.github.com>
2026-01-30 23:03:35 +0530

    Fix duplicate SAVED_API_END_POINT declaration

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,3 +3,4 @@ export const JOB_API_END_POINT=" https://opportunity-job-portal.onrender.com/api
 export const APPLICATION_API_END_POINT="https://opportunity-backend-f608.up.railway.app/api/v1/application";
 export const COMPANY_API_END_POINT="https://opportunity-backend-f608.up.railway.app/api/v1/company";
 export const SAVED_API_END_POINT = "https://opportunity-backend-f608.up.railway.app/api/v1/saved";
+export const SAVED_API_END_POINT = "https://opportunity-backend-f608.up.railway.app/api/v1/saved";
